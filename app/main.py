082bb0e7131932790de7c3b56f6fb532de2bdb2b
Vid R <ribvid@gmail.com>
2026-01-15 15:08:08 +0100

    wip

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
+
 from typing import Annotated
 from fastapi import Depends, FastAPI, UploadFile, HTTPException, BackgroundTasks
 from pathlib import Path
